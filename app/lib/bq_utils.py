bc54eb23d2749077430d01cc653d5e245e697818
Mike Castner <mjcastner@gmail.com>
2020-05-24 02:24:00 -0700

    Class inheritence work on BQ client, dataset and table construction placeholders

--- app/lib/bq_utils.py
+++ app/lib/bq_utils.py
@@ -1,6 +1,43 @@
+import json
+
+import boto3
+import google
+from absl import logging
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
-class bq_client(bigquery.client.Client):
-  def __init__(self):
-    self.banana = 'test'
 
+class aws_bq_client(bigquery.client.Client):
+  def __init__(self, aws_secret_key: str):
+    # Initialize BQ client from AWS Secrets Manager
+    secrets_manager = boto3.client('secretsmanager')
+    service_account_json = secrets_manager.get_secret_value(
+        SecretId=aws_secret_key).get('SecretString')
+    service_account_info = json.loads(service_account_json)
+    bq_credentials = service_account.Credentials.from_service_account_info(
+        service_account_info)
+    super().__init__(
+        credentials=bq_credentials,
+        project=bq_credentials.project_id)
+
+
+  def dataset(self, dataset_name: str):
+    try:
+      output_dataset = self.get_dataset(dataset_name)
+    except google.api_core.exceptions.NotFound as e:
+      logging.error('Dataset \'%s\' not found, creating...', dataset_name)
+      self.create_dataset(dataset_name)
+      output_dataset = self.get_dataset(dataset_name)
+
+    return output_dataset
+
+
+  def table(self, table_name: str):
+    try:
+      output_table = self.get_table(table_name)
+    except google.api_core.exceptions.NotFound as e:
+      logging.error('Table \'%s\' not found, creating...', table_name)
+      self.create_table(table_name)
+      output_table = self.get_table(table_name)
+
+    return output_table
--- app/sqs_to_bq.py
+++ app/sqs_to_bq.py
@@ -7,8 +7,8 @@ from absl import app
 from absl import flags
 from absl import logging
 from google.cloud import bigquery
-from google.oauth2 import service_account
 from lib import schema
+from lib import bq_utils
 
 # Define args
 FLAGS = flags.FLAGS
@@ -29,49 +29,40 @@ secrets_manager = boto3.client('secretsmanager')
 
 def stream_rows(bq_client: bigquery.client.Client, json_rows: str, 
                 table_name: str, dataset_name: str):
-  test = bq_client.get_dataset()
-
   return True
 
 
 def main(argv):
   # Instantiate BQ
-  service_account_json = secrets_manager.get_secret_value(
-      SecretId=FLAGS.service_account_secret).get('SecretString')
-  service_account_info = json.loads(service_account_json)
+  bq_client = bq_utils.aws_bq_client(FLAGS.service_account_secret)
+  dataset = bq_client.dataset('raxxla.edsm')
+  table = bq_client.table('raxxla.edsm.powerplay')
 
-  bq_credentials = service_account.Credentials.from_service_account_info(
-      service_account_info)
-  bq_client = bigquery.Client(
-      credentials=bq_credentials,
-      project=bq_credentials.project_id,
-  )
+  # # # Instantiate SQS
+  # sqs_queue = sqs.get_queue_by_name(QueueName=FLAGS.queue_name)
+  # available_messages = int(
+  #     sqs_queue.attributes.get('ApproximateNumberOfMessages'))
 
-  # # Instantiate SQS
-  sqs_queue = sqs.get_queue_by_name(QueueName=FLAGS.queue_name)
-  available_messages = int(
-      sqs_queue.attributes.get('ApproximateNumberOfMessages'))
+  # # # Process messages
+  # while available_messages > 0:
+  #   logging.info('%s messages available.', available_messages)
+  #   for message in sqs_queue.receive_messages(MaxNumberOfMessages=10,
+  #                                             MessageAttributeNames=['All']):
 
-  # # Process messages
-  while available_messages > 0:
-    logging.info('%s messages available.', available_messages)
-    for message in sqs_queue.receive_messages(MaxNumberOfMessages=10,
-                                              MessageAttributeNames=['All']):
+  #     table_name = message.message_attributes.get('table', {}).get(
+  #         'StringValue')
+  #     dataset_name = message.message_attributes.get('dataset', {}).get(
+  #         'StringValue')
 
-      table_name = message.message_attributes.get('table', {}).get(
-          'StringValue')
-      dataset_name = message.message_attributes.get('dataset', {}).get(
-          'StringValue')
+  #     streaming_status = stream_rows(
+  #         bq_client, message.body, table_name, dataset_name)
+  #     if streaming_status:
+  #       message.delete()
 
-      streaming_status = stream_rows(
-          bq_client, message.body, table_name, dataset_name)
-      if streaming_status:
-        message.delete()
+  #   available_messages = int(
+  #       sqs_queue.attributes.get('ApproximateNumberOfMessages'))
 
-    available_messages = int(
-        sqs_queue.attributes.get('ApproximateNumberOfMessages'))
-
-  logging.info('Queue processing complete!')
+  # logging.info('Queue processing complete!')
 
 
 if __name__ == '__main__':
