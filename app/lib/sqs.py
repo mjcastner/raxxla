9abb422f64c97affd96278560885d01b1e1c3ef5
Mike Castner <mjcastner@gmail.com>
2020-06-16 20:44:02 -0700

    Fix SQS class syntax

--- app/lib/sqs.py
+++ app/lib/sqs.py
@@ -6,7 +6,6 @@ sqs = boto3.client('sqs')
 class sqs_client(sqs):
   def __init__(self, queue_name: str, queue_url: str):
     # Initialize SQS client
-    super().__init__()
     self.queue = sqs.get_queue_by_name(QueueName=queue_name)
     self.available_messages = int(
         sqs_queue.attributes.get('ApproximateNumberOfMessages'))
