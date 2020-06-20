b04420c16d125c75c3f1737b44327385c65f4a93
Mike Castner <mjcastner@gmail.com>
2020-06-19 19:05:24 -0700

    First pass of EDSM refactor, woo!

--- app/edsm/main.py
+++ app/edsm/main.py
@@ -1,10 +1,7 @@
 import gzip
-import io
 import json
 import os
-import pprint
 import re
-import sys
 
 from multiprocessing import Pool
 from urllib import error as urllib_error
@@ -13,7 +10,6 @@ from urllib import request
 from absl import app, flags, logging
 from edsm import schema
 from lib import sqs
-
 
 # Global vars
 file_types_meta = schema.file_types.copy()
@@ -35,63 +31,68 @@ flags.mark_flag_as_required('type')
 flags.mark_flag_as_required('sqs_queue')
 
 
-def fetch_edsm_file(filetype: str) -> io.BufferedReader:
+def fetch_edsm_file(filetype: str) -> tuple:
   edsm_file_url = schema.urls.get(filetype)
   gz_filepath = '%s/%s.gz' % (FLAGS.download_dir, filetype)
 
-  # Fetch GZ from EDSM
-  logging.info('Fetching %s...', edsm_file_url)
   try:
+    logging.info('Fetching %s...', edsm_file_url)
     with request.urlopen(edsm_file_url) as response:
       gz_data = response.read()
 
       with open(gz_filepath, 'wb') as gz_file:
         gz_file.write(gz_data)
-        logging.info('Saved to %s...', gz_filepath)
+        logging.info('Saved to %s', gz_filepath)
   except (urllib_error.URLError, urllib_error.HTTPError) as e:
     logging.error('Error fetching %s file: %s', filetype, e)
 
-  return open(gz_filepath, 'rb')
+  return (filetype, gz_filepath)
 
 
-def process_edsm_file(filetype: str, gz_file: io.BufferedReader):
+def process_edsm_file(filetype: str, gz_filepath: str) -> bool:
+  gz_file = open(gz_filepath, 'rb')
   sqs_batch = []
 
-  with gzip.open(gz_file, mode='rt') as uncompressed_file:
-    for line in uncompressed_file:
-      if len(sqs_batch) < FLAGS.batch_size:
-        json_object_match = re.search(r'(\{.*\})', line)
-        if json_object_match:
-          json_object = json_object_match.group(1)
-          edsm_object = schema.edsmObject(filetype)
-          parsed_json = edsm_object.format_json(json_object)
-          sqs_batch.append(parsed_json)
-      else:
-        sqs_body = json.dumps(sqs_batch)
-        sqs_response = sqs.send_message(
-            queue_name=FLAGS.sqs_queue,
-            message_content=sqs_body,
-            message_attributes=edsm_object.attributes)
-        if sqs_response:
-          logging.info(
-              '[EDSM/%s] batch of %s sent to "%s" SQS queue.',
-              filetype,
-              len(sqs_batch),
-              FLAGS.sqs_queue)
-          sqs_batch.clear()
+  try:
+    with gzip.open(gz_file, mode='rt') as uncompressed_file:
+      for line in uncompressed_file:
+        if len(sqs_batch) < FLAGS.batch_size:
+          json_object_match = re.search(r'(\{.*\})', line)
+          if json_object_match:
+            json_object = json_object_match.group(1)
+            edsm_object = schema.edsmObject(filetype)
+            parsed_json = edsm_object.format_json(json_object)
+            sqs_batch.append(parsed_json)
+        else:
+          sqs_body = json.dumps(sqs_batch)
+          sqs_response = sqs.send_message(
+              queue_name=FLAGS.sqs_queue,
+              message_content=sqs_body,
+              message_attributes=edsm_object.attributes)
+          if sqs_response:
+            logging.info(
+                '[EDSM/%s] batch of %s sent to "%s" SQS queue',
+                filetype,
+                len(sqs_batch),
+                FLAGS.sqs_queue)
+            sqs_batch.clear()
 
-    sqs_body = json.dumps(sqs_batch)
-    sqs_response = sqs.send_message(
-        queue_name=FLAGS.sqs_queue,
-        message_content=sqs_body,
-        message_attributes=edsm_object.attributes)
-    if sqs_response:
-      logging.info(
-          '[EDSM/%s] batch of %s sent to "%s" SQS queue.',
-          filetype,
-          len(sqs_batch),
-          FLAGS.sqs_queue)
-      sqs_batch.clear()
+      sqs_body = json.dumps(sqs_batch)
+      sqs_response = sqs.send_message(
+          queue_name=FLAGS.sqs_queue,
+          message_content=sqs_body,
+          message_attributes=edsm_object.attributes)
+      if sqs_response:
+        logging.info(
+            '[EDSM/%s] batch of %s sent to "%s" SQS queue',
+            filetype,
+            len(sqs_batch),
+            FLAGS.sqs_queue)
+        sqs_batch.clear()
+      return True
+  except Exception as e:
+    logging.error(e)
+    return False
 
 
 def main(argv):
@@ -99,11 +100,24 @@ def main(argv):
 
   if FLAGS.type == 'all':
     with Pool(5) as fetch_pool:
-      edsm_file_objects = fetch_pool.map(fetch_edsm_file, schema.file_types)
-      #fetch_pool.map(process_edsm_file, edsm_file_objects)
+      fetch_responses = fetch_pool.map(fetch_edsm_file, schema.file_types)
+      for fetch_response in fetch_responses:
+        filetype = fetch_response[0]
+        gz_filepath = fetch_response[1]
+        process_response = process_edsm_file(filetype, gz_filepath)
+        if process_response:
+          logging.info('Successfully processed %s file!', FLAGS.type)
+          os.remove(gz_filepath)
+          logging.info('Temporary file %s removed', gz_filepath)
   else:
-    edsm_file_object = fetch_edsm_file(FLAGS.type)
-    process_edsm_file(FLAGS.type, edsm_file_object)
+    fetch_response = fetch_edsm_file(FLAGS.type)
+    gz_filepath = fetch_response[1]
+    process_response = process_edsm_file(FLAGS.type, gz_filepath)
+    if process_response:
+      logging.info('Successfully processed %s file!', FLAGS.type)
+      os.remove(gz_filepath)
+      logging.info('Temporary file %s removed', gz_filepath)
+
 
 
 if __name__ == '__main__':
