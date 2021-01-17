45c55ddbbadbe6a639ff6249d0bdf74875e3645e
Mike Castner <mjcastner@gmail.com>
2021-01-16 23:02:22 -0800

    Dataflow logic complete, only schema changes pending

--- app/edsm/batch.py
+++ app/edsm/batch.py
@@ -1,4 +1,3 @@
-import functools
 import gzip
 import io
 import json
@@ -6,9 +5,6 @@ import re
 import time
 import urllib.request
 import urllib.error
-
-from concurrent import futures
-from datetime import datetime
 
 from commonlib.google.dataflow import beam
 from commonlib.google import bigquery
@@ -20,16 +16,15 @@ from absl import app, flags, logging
 # Global vars
 DATASET = 'edsm'
 URLS = {
-    'bodies': 'https://www.edsm.net/dump/bodies7days.json.gz',
+    # 'bodies': 'https://www.edsm.net/dump/bodies7days.json.gz',
     'population': 'https://www.edsm.net/dump/systemsPopulated.json.gz',
     'powerplay': 'https://www.edsm.net/dump/powerPlay.json.gz',
-    'stations': 'https://www.edsm.net/dump/stations.json.gz',
-    'systems': 'https://www.edsm.net/dump/systemsWithCoordinates.json.gz',
+    # 'stations': 'https://www.edsm.net/dump/stations.json.gz',
+    # 'systems': 'https://www.edsm.net/dump/systemsWithCoordinates.json.gz',
 }
 FILE_TYPES = list(URLS.keys())
 FILE_TYPES_META = FILE_TYPES.copy()
 FILE_TYPES_META.append('all')
-DOWNLOAD_POOL = futures.ThreadPoolExecutor(max_workers=len(URLS))
 
 # Define flags
 FLAGS = flags.FLAGS
@@ -107,6 +102,7 @@ class FormatEdsmJson(apache_beam.DoFn):
         }
 
     def _rdictget(self, input_dict: dict, path: str):
+        import functools
         return functools.reduce(dict.get, path.split('.'), input_dict)
 
     def _rdictset(self, input_dict, path, value):
@@ -116,6 +112,7 @@ class FormatEdsmJson(apache_beam.DoFn):
         input_dict[keys[-1]] = value
 
     def _map_dict_fields(self, field_mappings):
+        from datetime import datetime
         for k, v in field_mappings.items():
             if k == 'timestamp_fields':
                 for ts_k, ts_v in v.items():
@@ -139,7 +136,6 @@ class FormatEdsmJson(apache_beam.DoFn):
         try:
             self.input_dict = json.loads(edsm_json)
             self._map_dict_fields(self.schema_mappings.get(self.file_type))
-
             yield json.dumps(self.output_dict)
         except Exception as e:
             logging.info(e)
@@ -151,28 +147,23 @@ def bigquery_loader(file_type: str, ndjson_file):
                                         DATASET, file_type, 'ndjson')
 
 
-def generate_ndjson_file(file_type: str, gcs_path: str):
+def execute_beam_pipeline(file_type: str, gcs_path: str):
     with apache_beam.Pipeline(
             options=beam.get_default_pipeline_options()) as p:
         pipeline_output = (p
                            | apache_beam.io.ReadFromText(gcs_path)
                            | apache_beam.ParDo(FormatEdsmJson(file_type))
-                           | apache_beam.Reshuffle()
                            | apache_beam.io.WriteToText(
                                'gs://%s/%s/%s_ndjson' %
                                (FLAGS.gcs_bucket, DATASET, file_type)))
-
-    ndjson_shards = gcs.get_blobs('%s/%s_ndjson' % (DATASET, file_type))
-    return gcs.combine_files(ndjson_shards,
-                             '%s/%s.ndjson' % (DATASET, file_type))
 
 
 def gcs_fetch(file_type: str, url: str):
     # Working GCS download
     # logging.info('Downloading and decompressing %s...', url)
     # file_blob = gcs.get_blob('%s/%s.gz' % (DATASET, file_type))
-    # gz_file_data = file_blob.download_as_bytes()
-    # decompressed_file = io.BytesIO(gzip.decompress(gz_file_data))
+    # gz_file_data = io.BytesIO(file_blob.download_as_bytes())
+    # decompressed_file = gzip.open(gz_file_data)
 
     # gcs_path = '%s/%s.json' % (DATASET, file_type)
     # logging.info('Uploading gs://%s...', gcs_path)
@@ -198,33 +189,34 @@ def main(argv):
     debug_start = time.time()
     gcs_files = []
 
-    if FLAGS.file_type == 'all':
-        logging.info('Fetching all EDSM files...')
-        edsm_file_blobs = list(
-            DOWNLOAD_POOL.map(gcs_fetch, FILE_TYPES, URLS.values()))
-        gcs_files.extend(edsm_file_blobs)
-
-        logging.info('Generating BigQuery tables...')
-        bigquery_tables = list(map(bigquery_loader, FILE_TYPES, ndjson_files))
-
-    else:
-        logging.info('Fetching %s from EDSM...', FLAGS.file_type)
-        edsm_file_blob = gcs_fetch(FLAGS.file_type, URLS[FLAGS.file_type])
+    def edsm_to_bigquery(file_type: str):
+        logging.info('Fetching %s from EDSM...', file_type)
+        edsm_file_blob = gcs_fetch(file_type, URLS[file_type])
         gcs_files.append(edsm_file_blob)
 
         logging.info('Generating NDJSON file via DataFlow %s...',
                      FLAGS.beam_runner)
-        edsm_ndjson_blob = generate_ndjson_file(
-            FLAGS.file_type, gcs.get_gcs_uri(edsm_file_blob.name))
+        execute_beam_pipeline(file_type, gcs.get_gcs_uri(edsm_file_blob.name))
+        ndjson_shards = gcs.get_blobs('%s/%s_ndjson' % (DATASET, file_type))
+        gcs_files.extend(ndjson_shards)
+        edsm_ndjson_blob = gcs.combine_files(
+            ndjson_shards, '%s/%s.ndjson' % (DATASET, file_type))
+        gcs_files.append(edsm_ndjson_blob)
 
-        logging.info('Generating BigQuery table at %s.%s', DATASET,
-                     FLAGS.file_type)
-        bigquery_table = bigquery_loader(FLAGS.file_type, edsm_ndjson_blob)
+        logging.info('Generating BigQuery table at %s.%s', DATASET, file_type)
+        return bigquery_loader(file_type, edsm_ndjson_blob)
+
+    if FLAGS.file_type == 'all':
+        logging.info('Fetching all EDSM files...')
+        edsm_bq_tables = [edsm_to_bigquery(x) for x in URLS.keys()]
+    else:
+        edsm_bq_table = edsm_to_bigquery(FLAGS.file_type)
+        print(edsm_bq_table.result())
 
     if FLAGS.cleanup_files:
+        logging.info('Cleaning up %s GCS file(s)...', len(gcs_files))
         [x.delete() for x in gcs_files]
 
-    DOWNLOAD_POOL.shutdown()
     debug_end = time.time()
     debug_duration = debug_end - debug_start
     logging.info('Pipeline completed in: %s', debug_duration)
