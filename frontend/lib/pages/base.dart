6311c2530079c53f5130e1b0fa915327dd9dec49
Mike Castner <mjcastner@gmail.com>
2021-01-18 00:18:52 -0800

    Add initial Flutter UI for frontend

---
+++ frontend/lib/pages/base.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class NavItem extends StatelessWidget {
+  String title;
+  String destination;
+  String image;
+
+  NavItem(String title, String destination, String image) {
+    this.title = title;
+    this.destination = destination;
+    this.image = image;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      onTap: () {
+        Navigator.pushNamed(context, this.destination);
+      },
+      leading: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: 30,
+          maxHeight: 30,
+        ),
+        child: Image.asset(this.image, fit: BoxFit.cover),
+      ),
+      title: Text(this.title),
+    ));
+  }
+}
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(children: [
+        DrawerHeader(
+          child: Row(children: [
+            Image.asset(
+              'images/universal_cartographics_logo.png',
+              height: 50,
+              width: 50,
+            ),
+            Text('Univeral Cartographics\nStellar Cartography Database',
+                style: GoogleFonts.roboto(fontWeight: FontWeight.bold))
+          ]),
+          decoration: BoxDecoration(
+            color: Colors.black87,
+          ),
+        ),
+        ExpansionTile(
+          title: Text('Codex'),
+          children: [
+            NavItem('Systems', '/systems', 'images/system.png'),
+            NavItem('Planets', '/planets', 'images/planet.png'),
+            NavItem('Settlements', '/systems', 'images/settlements.png'),
+            NavItem('Stations', '/systems', 'images/station.png'),
+          ],
+        ),
+        ExpansionTile(
+          title: Text('Galactic Census'),
+          children: [
+            NavItem('Population', '/systems', 'images/population.png'),
+            NavItem('Factions', '/systems', 'images/factions.png'),
+            NavItem('Powers', '/systems', 'images/powers.png'),
+          ],
+        ),
+        ExpansionTile(
+          title: Text('Technology'),
+          children: [
+            NavItem('Ships', '/systems', 'images/ship.png'),
+          ],
+        ),
+        ExpansionTile(
+          title: Text('Xenobiology'),
+          children: [
+            NavItem('Guardians', '/systems', 'images/guardian.png'),
+            NavItem('Thargoids', '/systems', 'images/thargoid.png'),
+          ],
+        ),
+      ]),
+    );
+  }
+}
---
+++ frontend/lib/pages/systems.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:raxxla/pages/base.dart';
+
+class SystemsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: NavDrawer(),
+      body: Center(child: Text('Systems')),
+    );
+  }
+}
